eb3e3299d513ea91e7afb9587e80cfc26ea7d61e
Vladimir Matyagin <ghost-email@yandex.ru>
2022-06-21 20:55:50 +0300

    * | revalidation api fix

--- pages/api/refetch.ts
+++ pages/api/refetch.ts
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { CHOICE_ROUTES } from 'src/features/block/consts';
 import { getEventIds } from 'src/features/event/api/module.api';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // choices
     await Promise.all(
-      ['1', '2', '3', '4'].map((choice) => res.unstable_revalidate(`/rating/choice/${choice}`))
+      Object.values(CHOICE_ROUTES).map((block) => res.unstable_revalidate(`/rating/block/${block}`))
     );
     const { eventIds } = await getEventIds();
-    await Promise.all(eventIds.map((eventId) => res.unstable_revalidate(`/rating/event/${eventId}`)));
+    await Promise.all(
+      eventIds.map((eventId) => res.unstable_revalidate(`/rating/event/${eventId}`))
+    );
 
     return res.json({ revalidated: true });
   } catch (err) {
